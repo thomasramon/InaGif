b94737df965a8e53714643aacda9a3ef1cfc5600
Ramon Thomas <thomasjramon@gmail.com>
2024-09-01 15:30:35 -0700

    Initial Commit

---
+++ InaGif/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InaGif
+//
+//  Created by Ramon Thomas on 9/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InaGif/InaGifApp.swift
@@ -0,0 +1,17 @@
+//
+//  InaGifApp.swift
+//  InaGif
+//
+//  Created by Ramon Thomas on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct InaGifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
